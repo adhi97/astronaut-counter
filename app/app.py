0bfce693df8d804bf1ee01ef00eed0e312737f57
Adheesh Maheshwari <a5mahesh@uwaterloo.ca>
2017-09-14 05:02:03 -0400

    basic weather interface

--- app/app.py
+++ app/app.py
@@ -1,14 +1,21 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Weather(Resource):
     def get(self):
-        return {'hello': 'world'}
+        print "getting weather data"
+        return #to be implemented (DAO class)
 
-api.add_resource(HelloWorld, '/')
+    def post(self):
+        print "posting weather data"
+        parser = reqparse.RequestParser()
+        return #success or failure
+
+
+api.add_resource(Weather, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
