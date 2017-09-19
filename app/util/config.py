881f2f99b31968c422c856d7d37160a7c05e2fea
Adheesh Maheshwari <adhi97@gmail.com>
2017-09-19 11:32:00 -0400

    adding os functionality to accomodate docker

--- app/util/config.py
+++ app/util/config.py
@@ -1,3 +1,6 @@
-db_name = 'astronaut'
-db_host = 'localhost'
-host_ip = 'localhost'
+import os
+
+db_name = os.environ['DB_NAME'] if 'DB_NAME' in os.environ else None
+collection_name = os.environ['COLLECTION_NAME'] if 'COLLECTION_NAME' in os.environ else None
+db_host = os.environ['DB_HOST'] if 'DB_HOST' in os.environ else None
+host_ip = os.environ['HOST_IP'] if 'HOST_IP' in os.environ else None
