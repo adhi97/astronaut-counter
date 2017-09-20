70ba961cd4d7fb299747147403b5034f594843eb
Adheesh Maheshwari <adhi97@gmail.com>
2017-09-20 08:55:52 -0400

    adding validators

--- app/validator.py
+++ app/validator.py
@@ -1,1 +1,11 @@
-#validator.py
+from voluptuous import Schema, Optional, Required, Length, All
+
+
+class Validator:
+	 astroSchema = Schema({
+    	Required('id'): All(str, Length(min=24, max=24))
+    })
+
+	@classmethod
+	def validateGet(cls, data):
+		return cls.astroSchema(data)
