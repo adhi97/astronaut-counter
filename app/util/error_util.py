b4e960cb3d28685f0406373b9daa638c02830a5b
Adheesh Maheshwari <adhi97@gmail.com>
2017-09-25 08:49:04 -0400

    adding more error util functions

--- app/util/error_util.py
+++ app/util/error_util.py
@@ -17,3 +17,48 @@ class ErrorUtil:
 class ErrorUtilBuilder:
 	def __init__(self):
 		self.errors = []
+
+	@classmethod
+    def bad_request(cls, msg='', errors=None):
+        cls.__handle_traceback__()
+
+        message = {
+            'status': 400,
+            'message': 'Bad Request: {}'.format(msg),
+            'errors': errors if errors else [],
+        }
+
+        resp = jsonify(message)
+        resp.status_code = 400
+
+        return resp
+
+    @classmethod
+    def unauthorized(cls, msg='', errors=None):
+        cls.__handle_traceback__()
+
+        message = {
+            'status': 401,
+            'message': 'Unauthorized: {}'.format(msg),
+            'errors': errors if errors else [],
+        }
+
+        resp = jsonify(message)
+        resp.status_code = 401
+
+        return resp
+
+    @classmethod
+    def not_found(cls, msg='', errors=None):
+        cls.__handle_traceback__()
+
+        message = {
+            'status': 404,
+            'message': 'Not Found: {}'.format(msg),
+            'errors': errors if errors else [],
+		}
+
+		resp = jsonify(message)
+		resp.status_code = 404
+
+		return resp
